cc52bce3d13a2fc64d819a200f33f57be9b77e1b
Adedamola Ajasa <azeemajasa@gmail.com>
2023-08-20 06:05:39 +0100

    new added features to display flatacuties UI and behaviour

---
+++ README.md
@@ -0,0 +1,58 @@
+# Flatacuties - Vote for the Cutest Animal!
+
+Flatacuties is a mini web app where you can vote for the cutest animal. It demonstrates array iteration, DOM manipulation, events handling, and server communication using a local API.
+
+
+## Project Setup
+
+To get started with Flatacuties, follow these steps:
+
+1. **Clone the Repository:** Clone this GitHub repository to your local machine.
+
+2. **Install Dependencies:** No external dependencies are required for this project.
+
+3. **Start the Local Server:**
+   - Create a `db.json` file in the project root directory and populate it with the provided character data.
+   - Open a terminal and navigate to the project root directory.
+   - Run the following command to start the JSON DB server:
+     ```sh
+     json-server --watch db.json
+     ```
+   - You can test the server by visiting `http://localhost:3000/characters` in your browser.
+
+4. **Open the App:** Open the `index.html` file in a modern web browser to start using the Flatacuties app.
+
+## Features
+
+- View a list of all animal names.
+- Click on an animal's name to see its details, including the image and number of votes.
+- Vote for your favorite animal, and the vote count will be displayed in real-time.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Project Structure
+
+- `index.html`: The main HTML file containing the app structure.
+- `styles.css`: The CSS file for styling the app.
+- `script.js`: The JavaScript file for handling data fetching, DOM manipulation, and event handling.
+
+## Usage
+
+1. Open the app in your web browser.
+2. You will see a list of animal names. Click on a name to view the animal's details.
+3. In the details view, you can vote for the animal by clicking the "Vote" button. The vote count will update instantly.
+
+## License
+
+This project is licensed under the MIT License. Feel free to use, modify, and distribute it as needed.
+
+## Acknowledgments
+
+- This project was created as a practice exercise to learn about array iteration, DOM manipulation, events, and server communication.
+
+---
+
---
+++ server/index.js
@@ -0,0 +1,93 @@
+const animalListContainer = document.querySelector('.animal-list');
+const animalDetailsContainer = document.querySelector('.animal-details');
+
+// To fetch animals data from the local server
+function fetchAnimals() {
+    return fetch('http://localhost:3000/characters')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch animals data');
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error(error);
+            return [];
+        });
+}
+
+// Display animal cards in the list
+function displayAnimalList() {
+    fetchAnimals()
+        .then(animals => {
+            animalListContainer.innerHTML = '';
+            
+            animals.forEach(animal => {
+                const card = document.createElement('div');
+                card.className = 'animal-card';
+                card.textContent = animal.name;
+                
+                card.addEventListener('click', () => {
+                    displayAnimalDetails(animal);
+                });
+                
+                animalListContainer.appendChild(card);
+            });
+        });
+}
+
+// To display details of a selected animal
+function displayAnimalDetails(animal) {
+    animalDetailsContainer.innerHTML = `
+        <h2>${animal.name}</h2>
+        <img src="${animal.image}" alt="${animal.name}">
+        <p>Votes: ${animal.votes}</p>
+        <button class="reset-btn">Reset Votes</button>
+        <button class="vote-btn">Vote</button>
+    `;
+    
+    // To update the vote count
+    const voteBtn = animalDetailsContainer.querySelector('.vote-btn');
+    voteBtn.addEventListener('click', () => {
+        animal.votes++;
+        updateAnimalVotes(animal.id, animal.votes)
+            .then(() => {
+                displayAnimalDetails(animal); 
+            });
+    });
+
+
+    // To reset the vote count
+    const resetBtn = animalDetailsContainer.querySelector('.reset-btn');
+    resetBtn.addEventListener('click', () => {
+        animal.votes = 0;
+        updateAnimalVotes(animal.id, 0)
+            .then(() => {
+                displayAnimalDetails(animal); 
+            });
+    });
+    
+    animalDetailsContainer.style.display = 'block';
+}
+
+// To update animal votes on the server
+function updateAnimalVotes(animalId, votes) {
+    return fetch(`http://localhost:3000/characters/${animalId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ votes })
+    })
+    .then(response => {
+        if (!response.ok) {
+            console.log('Failed to update animal votes');
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+// To start the app
+displayAnimalList();
